b9776fbe9eea750b83b135a6c7d52f6c6cf96f19
abhinavcodedeveloper <abhinavssr2003@gmail.com>
2021-11-20 13:49:50 +0530

    initial checking

---
+++ abcd.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+/*this is my first c prog which is awesome
+and amazing!!
+*/
+int main(){
+    int _tom;
+    int _Tom;
+    // i want paisa
+    int error_code;
+    printf("hello I started learning C");
+    return 0;
+}
---
+++ variables.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+ 
+int main()
+{
+    int a = 4;
+    // int b - 9.98; // not recommended 
+    float b = 9.97;
+    char c = 'A';
+    printf("the value of a is a");
+    return 0;
+}
